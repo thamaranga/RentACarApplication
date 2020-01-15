a9ee86a518f87c0132d52e124a34ea080e964766
Hasitha <hasithat@mobitel.lk>
2020-01-15 20:10:43 +0530

    Configured  eureka server and eureka clients

--- rentcloudcommons/src/main/java/com/hasitha/rentcloud/model/rent/Rent.java
+++ rentcloudcommons/src/main/java/com/hasitha/rentcloud/model/rent/Rent.java
@@ -1,4 +1,77 @@
 package com.hasitha.rentcloud.model.rent;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class Rent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int customerId;
+    private int vehicleId;
+    private String rentFrom;
+    private String rentTill;
+    private int currentOdometer;
+    private String returnLocation;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(int vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public int getCurrentOdometer() {
+        return currentOdometer;
+    }
+
+    public void setCurrentOdometer(int currentOdometer) {
+        this.currentOdometer = currentOdometer;
+    }
+
+    public String getReturnLocation() {
+        return returnLocation;
+    }
+
+    public void setReturnLocation(String returnLocation) {
+        this.returnLocation = returnLocation;
+    }
+
+    public String getRentFrom() {
+        return rentFrom;
+    }
+
+    public void setRentFrom(String rentFrom) {
+        this.rentFrom = rentFrom;
+    }
+
+    public String getRentTill() {
+        return rentTill;
+    }
+
+    public void setRentTill(String rentTill) {
+        this.rentTill = rentTill;
+    }
 }
