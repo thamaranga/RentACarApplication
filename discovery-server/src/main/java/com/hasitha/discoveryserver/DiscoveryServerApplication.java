b129199ff95efc49ca160dea6a522a16a0073191
Hasitha <hasithat@mobitel.lk>
2020-01-15 20:08:17 +0530

    Configured  eureka server and eureka clients

---
+++ discovery-server/src/main/java/com/hasitha/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,16 @@
+package com.hasitha.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//Below annotation is used to configure this service as a discovery service.
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
+
+}
